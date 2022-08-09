1ccf6cc1c6491d0b3bf2459724147af32847d432
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-08-09 18:20:14 +0530

    preference box update

--- lib/preference_widget.dart
+++ lib/preference_widget.dart
@@ -25,7 +25,7 @@ class _preference_widgetState extends State<preference_widget> with SingleTicker
   }
   @override
   initState(){
-    _controller = AnimationController(vsync: this,duration: Duration(seconds: 3));
+    _controller = AnimationController(vsync: this,duration: Duration(seconds: 1));
     _alignmentanimation = Tween<Alignment>(begin: Alignment.center,end: Alignment.topLeft).animate(CurvedAnimation(parent: _controller, curve: Interval(0.3, 1,curve: Curves.easeOut)));
     _sizeanimation = Tween<double>(begin: 300,end: 90).animate(CurvedAnimation(parent: _controller, curve: Interval(0.3, 1,curve: Curves.easeOut)));
     _controller.forward().then((value) => setState((){
@@ -37,6 +37,7 @@ class _preference_widgetState extends State<preference_widget> with SingleTicker
   @override
   Widget build(BuildContext context) {
     return Stack(
+      alignment: Alignment.center,
       children: [
         Container(
             child: AnimatedBuilder(
@@ -68,10 +69,11 @@ class _preference_widgetState extends State<preference_widget> with SingleTicker
             margin: EdgeInsets.only(top: 32,right: 15),
             height: 50,
             width: 220,
+            padding: EdgeInsets.only(left: 15),
             child: AnimatedOpacity(
               opacity: opacity,
               duration: Duration(seconds: 1),
-              child: Text("About you",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.white)),
+              child: Text("About you",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30,color: Colors.white)),
             ),
           ),
         ),
